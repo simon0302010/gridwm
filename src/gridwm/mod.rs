3c370d06ea06ec2155cef0348ea6d409b5d23c3c
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-11-04 20:44:54 +0100

    simple tiling layout

--- src/gridwm/mod.rs
+++ src/gridwm/mod.rs
@@ -7,7 +7,7 @@ use std::{
 };
 use x11::{
     xinerama,
-    xlib::{self, XWindowAttributes},
+    xlib::{self, XWindowAttributes, XkbEvent},
 };
 
 use thiserror::Error;
@@ -57,7 +57,12 @@ impl GridWM {
             xlib::XSelectInput(
                 self.display,
                 xlib::XDefaultRootWindow(self.display),
-                xlib::SubstructureRedirectMask | xlib::SubstructureNotifyMask,
+                xlib::SubstructureRedirectMask
+                    | xlib::SubstructureNotifyMask
+                    | xlib::KeyPressMask
+                    | xlib::KeyReleaseMask
+                    | xlib::ButtonPressMask
+                    | xlib::ButtonReleaseMask,
             );
         }
         Ok(())
@@ -78,6 +83,9 @@ impl GridWM {
                     xlib::UnmapNotify => {
                         self.remove_window(event);
                     }
+                    xlib::KeyPress => {
+                        self.handle_click(event);
+                    }
                     _ => {
                         debug!("event triggered: {:?}", event);
                     }
@@ -91,6 +99,7 @@ impl GridWM {
         let event: xlib::XMapRequestEvent = From::from(event);
         unsafe { xlib::XMapWindow(self.display, event.window) };
         self.windows.insert(event.window);
+        self.layout();
     }
 
     fn remove_window(&mut self, event: xlib::XEvent) {
@@ -103,6 +112,7 @@ impl GridWM {
                 warn!("tried removing not existing window")
             }
         }
+        self.layout();
     }
 
     fn get_screen_size(&self) -> Result<(i16, i16), GridWMError> {
@@ -134,5 +144,34 @@ impl GridWM {
             xlib::XGetWindowAttributes(self.display, window, &mut attrs);
         }
         attrs
+    }
+
+    fn handle_click(&self, event: xlib::XEvent) {
+        let event: xlib::XKeyPressedEvent = From::from(event);
+
+        let keysym = unsafe { xlib::XKeycodeToKeysym(self.display, event.keycode as u8, 0) as u32 };
+
+        match keysym {
+            x11::keysym::XK_space => {
+                debug!("Space pressed.");
+            }
+            _ => {}
+        }
+    }
+
+    fn layout(&self) {
+        if self.windows.is_empty() {
+            return;
+        }
+
+        if let Ok((screen_w, screen_h)) = self.get_screen_size() {
+            let window_width: i16 = screen_w / self.windows.iter().len() as i16;
+            let mut current_x = 0;
+            for window in &self.windows {
+                self.move_window(*window, current_x, 0);
+                self.resize_window(*window, window_width as u32, screen_h as u32);
+                current_x += window_width as i32;
+            }
+        }
     }
 }
